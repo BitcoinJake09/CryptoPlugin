bc18b38f2ceb638b7789835ae6265b20cf63a19c
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2021-05-20 22:20:02 -0400

    remove redis

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install -y oracle-java8-installer
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/oracle-jdk8-installer
-apt-get -y install redis-server
+#apt-get -y install redis-server
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
 echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main' > /etc/apt/sources.list.d/git.list
 
