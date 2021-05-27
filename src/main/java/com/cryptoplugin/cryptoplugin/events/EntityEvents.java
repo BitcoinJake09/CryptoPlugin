e0e8bdd5ed611a425cde336783db1a8c900dc290
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2021-05-26 20:12:59 -0400

    stability updates
    DOGEcoin tested and works, creates player addresses.
    /wallet shows players address balance, and fee estimates for transactions.
    /withdraw <address> <amount> works
    /tip <playername> <amount> works.
    should work with most BTC, LTC, DOGE codebase forks.

--- src/main/java/com/cryptoplugin/cryptoplugin/events/EntityEvents.java
+++ src/main/java/com/cryptoplugin/cryptoplugin/events/EntityEvents.java
@@ -35,14 +35,11 @@ public class EntityEvents implements Listener {
       for (ChatColor color : ChatColor.values()) {
         line = line.replaceAll("<" + color.name() + ">", color.toString());
       }
-      // add links
       final Pattern pattern = Pattern.compile("<link>(.+?)</link>");
       final Matcher matcher = pattern.matcher(line);
       matcher.find();
       String link = matcher.group(1);
-      // Right here we need to replace the link variable with a minecraft-compatible link
       line = line.replaceAll("<link>" + link + "<link>", link);
-
       rawwelcome.append(line);
     }
   }
@@ -61,8 +58,6 @@ public class EntityEvents implements Listener {
     try {
      	for (int x = 0; x < cryptoPlugin.NODES.size(); x++) {
       nodeWallet = new NodeWallet(player.getUniqueId().toString(), x);
-      player.sendMessage(ChatColor.YELLOW + "This server runs CryptoPlugin! ");
-      player.sendMessage(ChatColor.RED + "USE AT YOUR OWN RISK!");
       player.sendMessage(ChatColor.GREEN + "Your " + cryptoPlugin.NODES.get(nodeWallet.walletArray).CRYPTO_TICKER + " address is: " + cryptoPlugin.NODES.get(nodeWallet.walletArray).ADDRESS_URL + nodeWallet.address);
       System.out.println("Address for: " + player.getName() + " : " + nodeWallet.address);
       }
@@ -70,9 +65,9 @@ public class EntityEvents implements Listener {
       e.printStackTrace();
       player.sendMessage(ChatColor.RED + "couldnt load wallet.");
     }
-  
+      player.sendMessage(ChatColor.YELLOW + "This server runs CryptoPlugin! ");
+      player.sendMessage(ChatColor.RED + "USE AT YOUR OWN RISK!");
 
-    // On dev environment, admin gets op. In production, nobody gets op.
 
     final String ip = player.getAddress().toString().split("/")[1].split(":")[0];
     System.out.println("User " + player.getName() + "logged in with IP " + ip);
@@ -82,45 +77,10 @@ public class EntityEvents implements Listener {
     int tempWhichWallet = (Integer) cryptoPlugin.whichWallet.get(player.getUniqueId());
     System.out.println("wallet #" + tempWhichWallet +" enabled");
 
-/*
-	try {
-    if (cryptoPlugin.REDIS.exists("nodeAddress"+ player.getUniqueId().toString())) {
-	player.sendMessage(ChatColor.GREEN + "Your Deposit address on this server: " + cryptoPlugin.REDIS.get("nodeAddress"+ player.getUniqueId().toString()));
 
-      String url = cryptoPlugin.ADDRESS_URL + cryptoPlugin.REDIS.get("nodeAddress"+ player.getUniqueId().toString());
-      player.sendMessage(ChatColor.WHITE + "" + ChatColor.UNDERLINE + url);
-
-    } else {
-	cryptoPlugin.REDIS.set("nodeAddress"+ player.getUniqueId().toString(),cryptoPlugin.getAccountAddress(player.getUniqueId().toString()));
-	player.sendMessage(ChatColor.GREEN + "Your Deposit address on this server: " + cryptoPlugin.REDIS.get("nodeAddress"+ player.getUniqueId().toString()));
-
-      String url2 = cryptoPlugin.ADDRESS_URL + cryptoPlugin.REDIS.get("nodeAddress"+ player.getUniqueId().toString());
-      player.sendMessage(ChatColor.WHITE + "" + ChatColor.UNDERLINE + url2);
-
-	}
-	} catch(Exception E) {
-		    System.out.println(E);
-	}
-
-
-		try {
-		cryptoPlugin.updateScoreboard(player);
-		} catch (Exception excep) {
-			System.out.println(excep);
-		}
-
-*/
     String welcome = rawwelcome.toString();
     welcome = welcome.replace("<name>", player.getName());
     player.sendMessage(welcome);
-     
-
-    // Prints the user balance
-
-
-    
-    //CryptoPlugin.REDIS.zincrby("player:login", 1, player.getUniqueId().toString());
-
 
   }
 }
